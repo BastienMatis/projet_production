e55428b58d4b06c210950ec0af2f33104672e91e
alicegdev <71830893+alicegdev@users.noreply.github.com>
2023-07-05 15:48:47 +0200

    db: added sql script files and gitignore

---
+++ dbms/ddl/ddl.sql
@@ -0,0 +1,55 @@
+/* Schéma de données */
+use projet_production;
+
+CREATE TABLE user (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE admin (
+  userId INT NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+CREATE TABLE student (
+  userId INT NOT NULL,
+  studentIp VARCHAR(255),
+  studentUsername VARCHAR(255),
+  FOREIGN KEY (userId) REFERENCES user(id)
+);
+
+CREATE TABLE class (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE studentClass (
+  userId INT NOT NULL,
+  classId INT NOT NULL,
+  FOREIGN KEY (userId) REFERENCES user(id),
+  FOREIGN KEY (classId) REFERENCES classes(id)
+);
+
+CREATE TABLE challenge (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  command VARCHAR(255),
+  mistakeMsg VARCHAR(255),
+  challengeId INT NOT NULL,
+  FOREIGN KEY (challengeId) REFERENCES challenge(id)
+);
+
+CREATE TABLE classChallenge (
+  classId INT NOT NULL,
+  challengeId INT NOT NULL,
+  FOREIGN KEY (classId) REFERENCES Class(id),
+  FOREIGN KEY (challengeId) REFERENCES challenge(id)
+);
---
+++ dbms/ddl/init.sql
@@ -0,0 +1,9 @@
+/*
+Script de création de la base de données.
+*/
+create database IF NOT EXISTS projet_production;
+
+/* Créer l'utilisateur API */
+create user IF NOT EXISTS 'api-dev'@'%.%.%.%' identified by 'api-dev-password';
+grant select, update, insert, delete on test.* to 'api-dev'@'%.%.%.%';
+flush privileges;
