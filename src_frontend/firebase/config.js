9b4e9870b650612a3db343fd835f1527bbc1eed1
alicegdev <71830893+alicegdev@users.noreply.github.com>
2023-07-06 09:52:05 +0200

    frontend: bases of the vue project

---
+++ src_frontend/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATAWn2L74Az78_4RW-fbu97soAgzVe-Hs",
+    authDomain: "projet-production-96264.firebaseapp.com",
+    projectId: "projet-production-96264",
+    storageBucket: "projet-production-96264.appspot.com",
+    messagingSenderId: "625506099933",
+    appId: "1:625506099933:web:dec49758d317574329b134"
+  };
+
+const app = initializeApp(firebaseConfig);
