4366742bf39db0181df9cecbcede61bbd32cd2a7
gaburaisu <56201393+GaburaisuVGC@users.noreply.github.com>
2023-07-07 11:25:13 +0200

    Fixed auth controller and route, added cors (please npm i after the pull)

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,26 +1,25 @@
 import express from 'express';
-   import {
-     getAllUsers,
-     getUserById,
-     createUser,
-     updateUser,
-     deleteUser,
-   } from '../controllers/userController';
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/userController';
 
 import { loginAsAdmin } from '../controllers/authController';
 
-   const router = express.Router();
+const router = express.Router();
 
-   router.get('/', getAllUsers);
-   router.get('/:id', getUserById);
-   router.post('/', createUser);
-   router.put('/:id', updateUser);
-   router.delete('/:id', deleteUser);
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
-   // auth routes
-   router.post('/loginAsAdmin', (req) => {
-      const { username, password } = req.body;
-      loginAsAdmin(username, password);
-    });
+// Auth routes
+router.post('/loginAsAdmin', (req, res) => {
+  loginAsAdmin(req, res);
+});
 
-   export default router;
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes';
 import { DB } from './utility/DB';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors({
+  origin: ['http://localhost:8080']
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
