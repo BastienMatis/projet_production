b0424c16a06618c2c83cad264c075296f56938fa
Bastien Matis <matis.bastien@outlook.fr>
2023-07-10 21:59:54 +0200

    login and cleaner code

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,7 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { IUserRO } from "../model/User/IUser";
 import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { DB } from '../utility/DB';
-import { IRequestMagicLink, IRequestMagicLinkResponse } from "../types/auth/IRequestMagicLink";
 import { Email } from "../utility/Email";
 import { ApiError } from "../utility/Error/ApiError";
 import { ErrorCode } from "../utility/Error/ErrorCode";
@@ -42,8 +41,6 @@ export const magic = async (
     res: Response
   ): Promise<void> => {
     try {
-      const connection = await DB.Connection; // Assuming DB.Connection returns a valid connection object
-  
       const email = req.query.email;
       if (!email) {
         throw new ApiError(ErrorCode.BadRequest, 'auth/missing-email', "Email is missing in magic link request.");
@@ -83,101 +80,56 @@ export const magic = async (
   };
 
 
-// export const magic = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-//   try {
-//     const connection = await DB.Connection;
-//     const [rows] = await connection.query<RowDataPacket[]>('SELECT * FROM users WHERE users.email = ?');
-    // const email = req.body; // Récupérer les valeurs d'email à partir de la requête
-    // if (!email) {
-    //   throw new ApiError(ErrorCode.BadRequest, 'auth/missing-email', "Email is missing in magic link request.");
-    // }  
+  export const login = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    try {
+        if (!req.query.jwt) {
+            throw new ApiError(ErrorCode.BadRequest, 'auth/missing-magic-link-token', "Token is missing in login request.");
+    }
 
-//     // Utilisation d'une requête préparée pour éviter les injections SQL
+    const jwt = new JWT();
+    const decoded = await jwt.decode(req.query.jwt.toString(), {
+      issuer: ISSUER,
+      audience: MAGIC_AUD,
+    });
+
+    if (!decoded.id) {
+        throw new ApiError(ErrorCode.Unauthorized, 'auth/invalid-magic-link-token', "userId was not found in the payload for token");
+      }
+
+    const user = await Crud.Read<IUserRO>({
+    table: 'users',
+    idKey: 'id',
+    idValue: decoded.id,
+    columns: ['id']
+    }); 
     
-//     // const query = 'SELECT * FROM users WHERE users.email = ?';
-//     // const rows = await connection.query<RowDataPacket[]>(query, [email]);
-//     // console.log(rows)
+    let payload: IAccessToken = {
+        id: user.id
+      };
 
-//     if (rows[0].length > 0) {
-//       // Handle the case where no user with the specified email was found
-//       const user = rows[0][0];
-//       console.log(user);
-
-//     // Create the new JWT
-//     const jwt = new JWT();
-//     const encoded = await jwt.create(
-//       {
-//         id: user.id,
-//       },
-//       {
-//         expiresIn: '30 minutes',
-//         audience: MAGIC_AUD,
-//         issuer: ISSUER,
-//       }
-//     ) as string;
-
-//     const emailer = new Email();
+      const access = await jwt.create(payload, {
+        expiresIn: '12 hours',
+        issuer: ISSUER,
+        audience: ACCESS_AUD,
+      });
   
-//       const link = (process.env.FRONT_URL || 'http://localhost:' + (process.env.PORT)) + '/auth/login?jwt=' + encodeURIComponent(encoded);
-//       await emailer.sendMagicLink(email, link, 'Mon service');
+      const renew = await jwt.create(payload, {
+        expiresIn: '1 week',
+        issuer: ISSUER,
+        audience: RENEW_AUD,
+      });
   
-//       res.json({
-//         ok: true
-//       });
-//     } catch (err) {
-//         next(err)
-//   }; 
-// }
-
-// export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-//   try {
-//     const connection = await DB.Connection;
-//     if (!req.query.jwt) {
-//       throw new ApiError(ErrorCode.BadRequest, 'auth/missing-magic-link-token', "Token is missing in login request.");
-//     }
-
-//     const jwt = new JWT();
-//     const decoded = await jwt.decode(req.query.jwt.toString(), {
-//       issuer: ISSUER,
-//       audience: MAGIC_AUD,
-//     });
-
-//     if (!decoded.userId) {
-//       throw new ApiError(ErrorCode.Unauthorized, 'auth/invalid-magic-link-token', "userId was not found in the payload for token");
-//     }
-
-//     // Vérifier que l'utilisateur existe toujours
-//     const query = 'SELECT * FROM users WHERE users.id = ?';
-//     const [rows] = await connection.query<RowDataPacket[]>(query, [decoded.id]);
-
-//     if (rows.length === 0) {
-//       // Handle the case where no user with the specified email was found
-//     }
-//     const user = rows[0];
-
-//     let payload: IAccessToken = {
-//       id: user.id
-//     };
-
-//     const access = await jwt.create(payload, {
-//       expiresIn: '12 hours',
-//       issuer: ISSUER,
-//       audience: ACCESS_AUD,
-//     });
-
-//     const renew = await jwt.create(payload, {
-//       expiresIn: '1 week',
-//       issuer: ISSUER,
-//       audience: RENEW_AUD,
-//     });
-
-//     res.json({
-//       access: access,
-//       renew: renew,
-//       redirectTo: 'https://lien.vers.mon.front',
-//       message: 'Normalement ce endpoint va demander au navigateur de rediriger vers votre site ou ressource'
-//     });
-//   } catch (err) {
-//     next(err)
-//   }
-// }
+      res.json({
+        access: access,
+        renew: renew,
+        redirectTo: 'https://lien.vers.mon.front',
+        message: 'Normalement ce endpoint va demander au navigateur de rediriger vers votre site ou ressource'
+      });  
+    } catch (error) {
+        console.error('Error logging.', error);
+        res.status(500).json({ message: 'Error logging.' });
+      }
+  }
