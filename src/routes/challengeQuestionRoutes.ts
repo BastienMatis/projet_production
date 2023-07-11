d8bcdedf48a5b45ac183d2bfeeb042a8b37d2d0c
gaburaisu <56201393+GaburaisuVGC@users.noreply.github.com>
2023-07-11 21:48:58 +0200

    Update challengeQuestionRoutes.ts

--- src/routes/challengeQuestionRoutes.ts
+++ src/routes/challengeQuestionRoutes.ts
@@ -15,5 +15,6 @@ router.get('/:challengeId', getAllChallengeQuestions);
 router.post('/', createChallengeQuestion);
 router.put('/:id', updateChallengeQuestion);
 router.delete('/:id', deleteChallengeQuestion);
+router.post('/test/:challengeId', runChallengeTest);
 
 export default router;
