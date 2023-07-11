b580fb634e2e5c4ae9f4d718cc49cad03ae3c497
Bastien Matis <matis.bastien@outlook.fr>
2023-07-11 13:08:36 +0200

    validations

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
+
 import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { DB } from '../utility/DB';
+import { Crud } from "../utility/Crud";
+import { ApiError } from '../utility/Error/ApiError';
+import { ErrorCode } from '../utility/Error/ErrorCode'
+import { UserCreateValidator } from '../model/User/users.validator'
 
 export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -30,15 +35,19 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void> =>
   };
 
   export const createUser = async (req: Request, res: Response): Promise<void> => {
-    const { username, email } = req.body;
+    const user = req.body;
     console.log(req.body);
     try {
-      const connection = await DB.Connection;
-      const [result] = await connection.query<ResultSetHeader>(
-        'INSERT INTO users (username, email) VALUES (?, ?)',
-        [username, email]
-      );
-      res.json({ id: result.insertId, username, email });
+      if (!UserCreateValidator(user)) {
+        throw new ApiError(ErrorCode.BadRequest, 'validation/failed', 'Data did not pass validation', UserCreateValidator.errors);      
+      }
+
+      const result = await Crud.Create({
+        body: user, 
+        table: 'users'
+      });
+
+      res.json(result);
     } catch (error) {
       console.error('Error creating user:', error);
       res.status(500).json({ message: 'Error creating user.' });
@@ -46,19 +55,22 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void> =>
   };
   
   export const updateUser = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const { username, email } = req.body;
+    const id = req.params.id;
+    const update = req.body;
     try {
-      const connection = await DB.Connection;
-      const [result] = await connection.query<ResultSetHeader>(
-        'UPDATE users SET username = ?, email = ? WHERE id = ?',
-        [username, email, id]
-      );
-      if (result.affectedRows === 0) {
-        res.status(404).json({ message: 'User not found.' });
-      } else {
-        res.json({ id: Number(id), username, email });
+      if (!UserCreateValidator(update)) {
+        throw new ApiError(ErrorCode.BadRequest, 'validation/failed', 'Data did not pass validation', UserCreateValidator.errors);      
       }
+
+      const result = await Crud.Update({
+        body: update,
+        table: 'users',
+        idKey: 'id',
+        idValue: id
+      });
+
+      res.json(result);
+
     } catch (error) {
       console.error('Error updating user:', error);
       res.status(500).json({ message: 'Error updating user.' });
