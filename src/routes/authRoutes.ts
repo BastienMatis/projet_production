fad4322d855682eaf1661ac07809da19fdb0fba7
alicegdev <71830893+alicegdev@users.noreply.github.com>
2023-07-12 15:10:22 +0200

    added Mathieu's changes to register function

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { magic, login, loginAsAdmin } from '../controllers/authController';
+import { register, magic, login, loginAsAdmin } from '../controllers/authController';
 
 const ROUTES_AUTH = express.Router();
 
+ROUTES_AUTH.post('/register', register);
 ROUTES_AUTH.post('/loginAsAdmin', loginAsAdmin);
 ROUTES_AUTH.get('/magic', magic);
 ROUTES_AUTH.get('/login', login);
