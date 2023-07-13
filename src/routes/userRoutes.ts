be772143d24a946d49ef34d93ec1cf1f154d0553
alicegdev <71830893+alicegdev@users.noreply.github.com>
2023-07-13 16:35:16 +0200

    modified userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -26,17 +26,26 @@ router.post('/loginAsAdmin', (req, res) => {
 });
 
 // Student connection
-router.post('/connectToStudent', async (req, res) => {
+router.post('/sshLogin', async (req, res) => {
   try {
     await insertSSHInfo(req, res);
-    await insertStudentDBInfo(req, res);
-    await connectToStudentDatabase(1);
-
     console.log('Connexion à la base de données de l\'étudiant établie avec succès');
     res.sendStatus(200);
   } catch (error) {
     console.error('Erreur lors de la connexion à la base de données de l\'étudiant', error);
     res.status(500).json({ error: 'Erreur lors de la connexion à la base de données de l\'étudiant' });
+  }
+});
+
+router.post('/dbLogin', async (req, res) => {
+  try {
+    await insertStudentDBInfo(req, res);
+    await connectToStudentDatabase(1);
+    console.log('Connexion à la DB établie avec succès');
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Erreur lors de la connexion à la DB de l\'étudiant', error);
+    res.status(500).json({ error: 'Erreur lors de la connexion à la DB de l\'étudiant' });
   }
 });
 
